85362e5e33be1abb29e443744a431369a7950c09
anatole-P <72392172+anatole-P@users.noreply.github.com>
2020-11-02 16:17:41 +0100

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,14 @@
+alert("Cc la tcheam");
+
+
+let largeur=$("#balle").width();
+let gauche=parseInt("#balle").css("left");
+let haut=parseInt(#balle).css("top");
+alert(largeur)
+
+setInterval(function() => {
+    gauche=gauche+1;
+    haut=haut+0.5;
+    $:("#balle").css("left",gauche)
+    $:("#balle").css("top",haut)
+}, 10)
