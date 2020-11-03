e6c095630172baf5c685c5b75f14c83e65592745
anatole-P <72392172+anatole-P@users.noreply.github.com>
2020-11-03 18:27:17 +0100

    Update js.js

--- js/js.js
+++ js/js.js
@@ -2,13 +2,13 @@ alert("Cc la tcheam");
 
 
 let largeur=$("#balle").width();
-let gauche=parseInt("#balle").css("left");
-let haut=parseInt(#balle).css("top");
-alert(largeur)
+let gauche=parseInt($("#balle").css("left"));
+let haut=parseInt($("#balle").css("top"));
+alert(largeur);
 
-setInterval(function() => {
-    gauche=gauche+1; b
+setInterval(function(){
+    gauche=gauche+1;
     haut=haut+0.5;
-    $:("#balle").css("left",gauche)
-    $:("#balle").css("top",haut)
-}, 10)
+    $("#balle").css("left",gauche);
+    $("#balle").css("top",haut);
+}, 10);
