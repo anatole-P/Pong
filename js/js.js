e0eb7c92bbe7d8b4424aaabcd6f508533816315a
anatole-P <72392172+anatole-P@users.noreply.github.com>
2020-11-03 19:40:13 +0100

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,14 +1,14 @@
-let largeur =$("#ball").width();
-let gauche = parseInt(($("#ball").css("left")));
-let haut= parseInt(($("#ball").css("top")));
+let largeur =$("#balle").width();
+let gauche = parseInt(($("#balle").css("left")));
+let haut= parseInt(($("#balle").css("top")));
 alert(gauche);
 
 let i=0;
 setInterval(function(){
     gauche = gauche + 1;
     haut = haut + 1;
-    $("#ball").css("left",gauche);
-    $("#ball").css("top",haut)
+    $("#balle").css("left",gauche);
+    $("#balle").css("top",haut)
     if(gauche >= 800){
         alert("ball dehors")
     }
